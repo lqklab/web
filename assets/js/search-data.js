6c619e5ab4af5c2f6dc08e5d56ab50673540f8ae
TDA-2030 <39872773+TDA-2030@users.noreply.github.com>
2025-11-20 08:28:43 +0000

    Deploying to gh-pages from @ lqklab/web@b0285ea9b411c827ece929b91cf96b74368d2217 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -290,17 +290,6 @@ ninja.data = [{
             window.location.href = "/blog/2022/giscus-comments/";
           
         },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
       },{id: "post-a-post-with-redirect",
         
           title: "a post with redirect",
@@ -405,6 +394,16 @@ ninja.data = [{
           description: "9.10 教师节登山",
           section: "News",handler: () => {
               window.location.href = "/news/group-activities/";
+            },},{id: "news-研电赛一等奖",
+          title: '研电赛一等奖',
+          description: "8.10 第19届研电赛总决赛",
+          section: "News",handler: () => {
+              window.location.href = "/news/competition/";
+            },},{id: "news-iros2025学术会议",
+          title: 'IROS2025学术会议',
+          description: "10.20 IROS学术会议",
+          section: "News",handler: () => {
+              window.location.href = "/news/IROS2025/";
             },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
